b747ad0cd890a974d8cc9fea6bde2768a46d07ea
NickNSx <nickogir@gmail.com>
2022-09-30 20:10:48 -0500

    Edit exceptions

--- src/main/java/com/project/common/ErrorResponseAspect.java
+++ src/main/java/com/project/common/ErrorResponseAspect.java
@@ -6,6 +6,8 @@ import java.time.format.DateTimeFormatter;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -25,10 +27,23 @@ public class ErrorResponseAspect {
     DateTimeFormatter format = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
 
 
-    @ExceptionHandler({InvalidRequestException.class, JsonMappingException.class})
+    @ExceptionHandler({InvalidRequestException.class, JsonMappingException.class, HttpMessageNotReadableException.class, HttpMediaTypeNotSupportedException.class})
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleBadRequests(Exception e) {
         logger.warn("A bad request was received at {}, details: {}", LocalDateTime.now(), e.getMessage());
+        if (e.toString().contains("UpdateUserRequest")) {
+            return new ErrorResponse(400, "Unrecognized entry. Enter reimbId to update and information to update: amount, description, type, or status");
+        }
+        if (e.toString().contains("NewReimbRequest")) {
+            return new ErrorResponse(400, "New ReimbRequest error");
+        }
+        if (e.toString().contains("UpdateUserRequest")) {
+            return new ErrorResponse(400, "Unrecognized entry. Enter userId to update and information to update: username, givenName, surname, " +
+                                         "email, isActive, or role.");
+        }
+        if (e.toString().contains("NewUserRequest")) {
+            return new ErrorResponse(400, "New User Request error");
+        }
         return new ErrorResponse(400, e.getMessage());
     }
 
@@ -50,7 +65,7 @@ public class ErrorResponseAspect {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse handleResourceNotFoundExceptions(ResourceNotFoundException e) {
         logger.warn("A ResourceNotFoundException occurred at {}, details: {}", LocalDateTime.now(), e.getMessage());
-        return new ErrorResponse(403, e.getMessage());
+        return new ErrorResponse(404, e.getMessage());
     }
 
     @ExceptionHandler
@@ -70,6 +85,7 @@ public class ErrorResponseAspect {
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorResponse handleOtherExceptions(Exception e) {
+        e.printStackTrace();
         logger.error("A unhandled exception was thrown at {}, details: {}", LocalDateTime.now(), e.getMessage());
         return new ErrorResponse(500, "An unexpected exception occurred. Devs, please check logs.");
     }
--- src/main/java/com/project/common/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/project/common/exceptions/ResourceNotFoundException.java
@@ -5,4 +5,8 @@ public class ResourceNotFoundException extends RuntimeException{
     public ResourceNotFoundException() {
         super("No resource found using the provided search parameters.");
     }
+
+    public ResourceNotFoundException (String message) {
+        super(message);
+    }
 }
