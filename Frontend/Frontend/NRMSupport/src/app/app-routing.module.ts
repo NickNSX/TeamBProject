aad28088f0b6c9af973edae65fee748dab2746bc
NickNSx <nickogir@gmail.com>
2022-10-14 14:48:26 -0500

    Added login component

--- Frontend/Frontend/NRMSupport/src/app/app-routing.module.ts
+++ Frontend/Frontend/NRMSupport/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
 
   {path:"register",component:RegisterComponent},
+  {path:"",component:LoginComponent},
 ];
 
 @NgModule({
