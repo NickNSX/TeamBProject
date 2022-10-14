ddc56889b7e69312a97522c17ca974111ed3445f
NickNSx <nickogir@gmail.com>
2022-10-14 14:21:52 -0500

    Added User's services to connect to backend

--- Frontend/Frontend/NRMSupport/src/app/services/user.service.ts
+++ Frontend/Frontend/NRMSupport/src/app/services/user.service.ts
@@ -1,9 +1,41 @@
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  user:User = {
+    username:"",
+    password:""
+  }
+
+  login(userCreds:User):Observable<HttpResponse<User>> {
+    return this.http.post("http://localhost:5000/reimb/auth/", userCreds) as Observable<HttpResponse<User>>;
+  }
+
+  search(idToSearch:string):Observable<HttpResponse<User>> {
+    return this.http.get("http://localhost:5000/reimb/users/" + idToSearch) as Observable<HttpResponse<User>>;
+  }
+
+  deactivate(userId:string) {
+    return this.http.delete("http://localhost:5000/reimb/users/" + userId);
+  }
+
+  updateUser(updateUser:User) {
+    return this.http.put("http://localhost:5000/reimb/users/", updateUser);
+  }
+
+  register(newUser:User) {
+    return this.http.post("http://localhost:5000/reimb/users/", newUser);
+  }
+
+  logOut () {
+    this.user = { }
+  }
 }
