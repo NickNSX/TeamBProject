e94e015815c885e8c277528409723d5b20d129e7
NickNSx <nickogir@gmail.com>
2022-10-14 15:01:45 -0500

    Added login component

--- Frontend/Frontend/NRMSupport/src/app/components/login/login.component.ts
+++ Frontend/Frontend/NRMSupport/src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private us:UserService) { }
 
   ngOnInit(): void {
+  }
+
+  message:string = "Login";
+  invalidCred:boolean = true;
+
+  user:User = { };
+
+  submitData () {
+    
+    this.us.login(this.user).subscribe(
+
+      (data:any) => {
+        
+        this.us.user = data;
+        this.route.navigate(['users']);
+      },
+
+      () => {
+        this.message = "Failed";
+        this.invalidCred = false;
+        this.user.username = "";
+        this.user.password = "";
+      }
+    );
   }
 
 }
