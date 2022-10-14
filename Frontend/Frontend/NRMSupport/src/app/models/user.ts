d4753c7af2a5b3f3f19aaa254cf25308fdef9575
NickNSx <nickogir@gmail.com>
2022-10-14 14:19:02 -0500

    Added user's variables

--- Frontend/Frontend/NRMSupport/src/app/models/user.ts
+++ Frontend/Frontend/NRMSupport/src/app/models/user.ts
@@ -1,2 +1,13 @@
 export class User {
+
+    constructor (
+        public username?:string,
+        public email?:string,
+        public givenName?:string,
+        public surname?:string,
+        public userId?:string,
+        public role?:string,
+        public isActive?:boolean,
+        public password?:string
+    ) {}
 }
