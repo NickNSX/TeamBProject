4634cd54dfb7a9838b9feb89a8cf55c1ff9e657f
NickNSx <nickogir@gmail.com>
2022-10-14 14:34:25 -0500

    Added registration form

--- Frontend/Frontend/NRMSupport/src/app/components/register/register.component.ts
+++ Frontend/Frontend/NRMSupport/src/app/components/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private us:UserService) { }
 
   ngOnInit(): void {
+  }
+
+  error:boolean = true;
+  success:boolean = true;
+  errorMessage:string = "";
+  idMessage:string = "";
+
+  registerUser:User = { };
+
+  register() {
+    console.log(this.registerUser);
+    this.us.register(this.registerUser).subscribe(
+      (data:any) => {
+        console.log(data);
+        this.error = true;
+        this.success = false;
+        this.idMessage = data.resourceId;
+      },
+      err => {
+        console.log(err.error.message);
+        this.error = false;
+        this.errorMessage = err.error.message + "!";
+      }
+      );
   }
 
 }
