0b3df33576693ff8268fea228065ea21290caef5
Regina Marsh <rema0813@yahoo.com>
2022-10-15 04:48:41 -0500

    updating reimbursement class functions

--- Frontend/Frontend/NRMSupport/src/app/app-routing.module.ts
+++ Frontend/Frontend/NRMSupport/src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { ReimbursementsComponent } from './components/reimbursements/reimbursements.component';
 
 const routes: Routes = [
 
   {path:"register",component:RegisterComponent},
   {path:"",component:LoginComponent},
+  
+  {path:"reimbursements",
+  component:ReimbursementsComponent}
 ];
 
 @NgModule({
