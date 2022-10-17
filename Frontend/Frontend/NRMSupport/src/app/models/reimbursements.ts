4945d5448acc72b81b578642b74e3dac700ddab3
Regina Marsh <rema0813@yahoo.com>
2022-10-17 10:19:42 -0500

    Update to files

--- Frontend/Frontend/NRMSupport/src/app/models/reimbursements.ts
+++ Frontend/Frontend/NRMSupport/src/app/models/reimbursements.ts
@@ -1,2 +1,33 @@
 export class Reimbursements {
-}
+
+    constructor( 
+
+        public reimb_id:any,
+    
+        public amount:any,
+    
+        public submitted:any,
+    
+        public resolved:any,
+    
+        public description:any,
+    
+        public author_id:any,
+    
+        public resolver_id:any,
+    
+        public status_id:any,
+    
+        public type_id:any)  {
+    
+       }
+
+    
+    
+    }
+    
+
+
+
+
+
