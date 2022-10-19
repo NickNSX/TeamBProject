ba40ab24f70a95f1ffed38ab6a8a5fbf2524dcc3
Max G <max582@revature.net>
2022-10-18 22:17:24 -0400

    reim coonected to backe/ show all reib from database

--- Frontend/Frontend/NRMSupport/src/app/components/inputbox/inputbox.component.ts
+++ Frontend/Frontend/NRMSupport/src/app/components/inputbox/inputbox.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Reimbursements } from 'src/app/models/reimbursements';
+import { UserService } from 'src/app/services/user.service';
 import {ReimbursementServiceTsService} from '../../services/reimbursement.service.ts.service';
 
 @Component({
@@ -14,18 +15,18 @@ export class InputboxComponent implements OnInit {
   remiArray:any = [ ];
 
   ReimSingle:Reimbursements = {
-    reimbId: undefined,
+    id: undefined,
     amount: undefined,
     submitted: undefined,
     resolved: undefined,
     description: undefined,
     authorId: undefined,
     resolverId: undefined,
-    statusId: undefined,
-    typeId: undefined
+    status: undefined,
+    type: undefined
   };
 
-  constructor(private rs: ReimbursementServiceTsService) { }
+  constructor(private rs: ReimbursementServiceTsService, private us: UserService) { }
   hiddenGrid: boolean = true;
   ngOnInit(): void {
     this.hiddenGrid = true;
@@ -43,7 +44,14 @@ export class InputboxComponent implements OnInit {
 
   callingServiceForRei(){
   
-    // this.rs.getRemb(this.authorID).subscribe(
+
+    if(this.remiArray.length > 1)
+    {
+      for( let i =this.remiArray.length;i >=0 ;i-- ){
+        this.remiArray.pop()
+    }
+  }
+    // this.rs.getReimbursement().subscribe(
     //   (data:any) => {console.log(data)
     //     for(let i = 0; i<data.length; i++)
     //     {
@@ -55,6 +63,42 @@ export class InputboxComponent implements OnInit {
 
     
   }
+
+  getAllReim(){
+
+    if(this.remiArray.length > 0)
+    {
+      for( let i =this.remiArray.length;i >=0 ;i-- ){
+        this.remiArray.pop()
+      }
+  }
+  if(this.us.user.role == "Finance Manager")
+  {
+    this.rs.getReimbursement("/manager").subscribe(
+      (data:any) => {console.log(data)
+        for(let i = 0; i<data.length; i++)
+        {
+          this.remiArray.push(data[i]);
+        }
+      
+      }
+    )
+  }else if(this.us.user.role != "Finance Manager" && this.us.user.role != undefined)
+  {
+    this.rs.getReimbursement("/employee/"+this.us.user.userId).subscribe((data:any) => {console.log(data)
+      for(let i = 0; i<data.length; i++)
+      {
+        this.remiArray.push(data[i]);
+      }
+    
+    })
+  }
+
+  }
+
+
+
+
 
 }
 
